693ae41d9f691607038c12b5efde922829d24cc3
w2j1y12 <62472117+w2j1y12@users.noreply.github.com>
2021-10-25 15:58:56 +0900

    Add files via upload

--- py-streamlit/st-mid-exam/heart_EDA_pdm17.py
+++ py-streamlit/st-mid-exam/heart_EDA_pdm17.py
@@ -15,44 +15,44 @@ df0 = pd.read_csv("https://github.com/Redwoods/Py/raw/master/pdm2020/my-note/py-
 # 1. Check zeros in features with Pregnancies, Outcome excluded.
 # 2. Replace zero with NaN 
 # 3. Replace NaN with the median of the corresponding featurs
-# def zero2median(df):
-#     columns_with_zero = df.columns[(df==0).sum() > 0][1:-1]
-#     # Index(['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI'], dtype='object')
-#     df[columns_with_zero]=df[columns_with_zero].replace(0,np.nan)
-#     for feature in columns_with_zero:
-#         df[feature].fillna(df[feature].median(),inplace=True)
+def zero2median(df):
+    columns_with_zero = df.columns[(df==0).sum() > 0][1:-1]
+    # Index(['Glucose', 'BloodPressure', 'SkinThickness', 'Insulin', 'BMI'], dtype='object')
+    df[columns_with_zero]=df[columns_with_zero].replace(0,np.nan)
+    for feature in columns_with_zero:
+        df[feature].fillna(df[feature].median(),inplace=True)
     
-#     return df
+    return df
 
 # Start with cleaned dataframe (이미 전처리 되었음.)
-df = df  # zero2median(df0)
+df = zero2median(df0)
 
 # 정상인(noHD), 환자(HD) 구분은 'target' 변수
 # [hint] 'Outcome' => 'target'
 # [hint] 'noDM' => 'noHD'  ,    'DM' => 'HD'
-df['Outcome'] = df['Outcome'].apply(lambda x: 'DM' if x == 1 else 'noDM')
+df['target'] = df['target'].apply(lambda x: 'HD' if x == 1 else 'noHD')
 
-classes=df.Outcome
-noDB,DB=classes.value_counts()
-st.sidebar.write('non-Heart-Disease(noHD):',noDB)
-st.sidebar.write('Heart-Disease(HD):',DB)
+classes=df.target
+noHD,HD=classes.value_counts()
+st.sidebar.write('non-Heart-Disease(noHD):',noHD)
+st.sidebar.write('Heart-Disease(HD):',HD)
 
 # plotly batchart
 # https://towardsdatascience.com/step-by-step-bar-charts-using-plotly-express-bb13a1264a8b
-df1 = df.groupby(["Outcome"]).count().reset_index()
+df1 = df.groupby(["target"]).count().reset_index()
 fig0 = px.bar(df1, 
-              y=df.groupby(["Outcome"]).size(),
-              x="Outcome", 
+              y=df.groupby(["target"]).size(),
+              x="target", 
               width=350, height=300, 
               labels={'y':'Number'}, 
-              color='Outcome', 
-              color_discrete_map=dict(noDM = 'green', DM = 'red'))
+              color='target', 
+              color_discrete_map=dict(noHD = 'green', HD = 'red'))
 st.sidebar.write(fig0)
 # pi-chart
-fig1=px.pie(df, values=df.Outcome.value_counts(), 
+fig1=px.pie(df, values=df.target.value_counts(), 
             width=350, height=300, 
-            names=['noDM','DM'], color=['noDM','DM'], 
-            color_discrete_map=dict(noDM = 'green', DM = 'red')) 
+            names=['noHD','HD'], color=['noHD','HD'], 
+            color_discrete_map=dict(noHD = 'green', HD = 'red')) 
 st.sidebar.write(fig1)
 
 st.subheader(f'Clean Data Information: shape = {df.shape}')
@@ -73,40 +73,40 @@ row0_1, row0_space2, row0_2 = st.beta_columns(
     (1, .1, 1))
 
 with row0_1: 
-    hist_x = st.selectbox("Select a feature", options=df.columns, index=df.columns.get_loc("Pregnancies"))
+    hist_x = st.selectbox("Select a feature", options=df.columns, index=df.columns.get_loc("target"))
         
 with row0_2: 
     bar_mode = st.selectbox("Select barmode", ["relative", "group"], 0)
 
 hist_bins = st.slider(label="Histogram bins", min_value=5, max_value=50, value=25, step=1, key='h1')
-# hist_cats = df['Outcome'].sort_values().unique()
+hist_cats = df['target'].sort_values().unique()
 hist_cats = df[hist_x].sort_values().unique()
 hist_fig1 = px.histogram(df, x=hist_x, nbins=hist_bins, 
                          title="Histogram of " + hist_x,
                          template="plotly_white", 
-                         color='Outcome', 
+                         color='target', 
                          barmode=bar_mode, 
-                         color_discrete_map=dict(noDM = 'green', DM = 'red'),  
+                         color_discrete_map=dict(noHD = 'green', HD = 'red'),  
                          category_orders={hist_x: hist_cats}) 
 st.write(hist_fig1)
 
 
 # boxplots
 st.header("Boxplot")
-st.subheader("With a categorical variable - Outcome [noDM, DM]")
+st.subheader("With a categorical variable - target [noHD, HD]")
 ## multi-column layput
 row1_1, row1_space2, row1_2 = st.beta_columns(
     (1, .1, 1))
 
 with row1_1: 
-    box_x = st.selectbox("Boxplot variable", options=df.columns, index=df.columns.get_loc("Age"))
+    box_x = st.selectbox("Boxplot variable", options=df.columns, index=df.columns.get_loc("age"))
         
 with row1_2: 
-    box_cat = st.selectbox("Categorical variable", ["Outcome"], 0)
+    box_cat = st.selectbox("Categorical variable", ["target"], 0)
 
 st.write("Hint - try comparison w.r.t Categories")
-box_fig = px.box(df, x=box_cat, y=box_x, title="Box plot of " + box_x, color='Outcome', 
-                        color_discrete_map=dict(noDM = 'green', DM = 'red'), 
+box_fig = px.box(df, x=box_cat, y=box_x, title="Box plot of " + box_x, color='target', 
+                        color_discrete_map=dict(noHD = 'green', HD = 'red'), 
                         template="plotly_white", 
                         points="all") 
 st.write(box_fig)
@@ -117,28 +117,28 @@ st.header("Correlations")
 row2_1, row2_space2, row2_2 = st.beta_columns((1, .1, 1))
 
 with row2_1: 
-    corr_x = st.selectbox("Correlation - X variable", options=df.columns, index=df.columns.get_loc("SkinThickness"))
+    corr_x = st.selectbox("Correlation - X variable", options=df.columns, index=df.columns.get_loc("cp"))
         
 with row2_2: 
-    corr_y = st.selectbox("Correlation - Y variable", options=df.columns, index=df.columns.get_loc("BMI"))
+    corr_y = st.selectbox("Correlation - Y variable", options=df.columns, index=df.columns.get_loc("chol"))
 
 corr2_fig = px.scatter(df, x=corr_x, y=corr_y, 
-                            color='Outcome', 
-                            color_discrete_map=dict(noDM = 'green', DM = 'red'), 
+                            color='target', 
+                            color_discrete_map=dict(noHD = 'green', HD = 'red'), 
                             template="plotly_white")
 st.write(corr2_fig)
 
 # heatmap
 st.subheader('Heatmap of correlation')
-fig4 = plt.figure(figsize=(6,5))
+fig4 = plt.figure(figsize=(12,10))
 sns.heatmap(df.corr(),annot=True, vmin=-1, vmax=1, cmap='coolwarm')
 st.pyplot(fig4)
-# st.write(fig4)
+st.write(fig4)
 
 # correlation heatmap
 st.subheader('Heatmap of selected parameters')
-fig5 = plt.figure(figsize=(5,4))
-hmap_params = st.multiselect("Select parameters to include on heatmap", options=list(df.columns), default=[p for p in df.columns if "Outcome" not in p])
+fig5 = plt.figure(figsize=(10,8))
+hmap_params = st.multiselect("Select parameters to include on heatmap", options=list(df.columns), default=[p for p in df.columns if "target" not in p])
 sns.heatmap(df[hmap_params].corr(), annot=True, vmin=-1, vmax=1, cmap='coolwarm')
 st.pyplot(fig5)
 
